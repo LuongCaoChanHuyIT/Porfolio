cdf2277bcba5dff9cce6166a85ba4bb50dfcac87
Lương Cao Chấn Huy <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2025-08-27 20:52:20 +0700

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- <Navbar />
+    <Header />
+    <About />
+    <Projects />
+    <Contact />
+    <Footer /> -->
+    hello
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import Header from '@/components/Header.vue'
+import About from '@/components/About.vue'
+import Projects from '@/components/Projects.vue'
+import Contact from '@/components/Contact.vue'
+import Footer from '@/components/Footer.vue'
+</script>
