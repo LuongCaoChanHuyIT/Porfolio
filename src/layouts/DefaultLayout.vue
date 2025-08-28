7c5a498e11122588ebc2677b29935075887de4ce
Lương Cao Chấn Huy <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2025-08-28 09:39:44 +0700

    final: component work, font style bug

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+      <Navigation />
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navigation from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<style scoped>
+</style>
